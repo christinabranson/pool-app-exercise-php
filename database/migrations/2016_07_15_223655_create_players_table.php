851f1ee013b826005e8d421e8ab4dab3253dcff8
Christina Branson <clbranson@gmail.com>
2016-07-15 22:40:48 +0000

    Added migration for players and games tables

---
+++ database/migrations/2016_07_15_223655_create_players_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->increments('player_id');
+            $table->string('name')->unique();
+            $table->integer('wins');
+            $table->integer('losses');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('players');
+    }
+}
---
+++ database/migrations/2016_07_15_223704_create_games_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->increments('game_id');
+            $table->integer('player_1_id');
+            $table->integer('player_2_id');
+            $table->integer('winner_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('games');
+    }
+}
