8c6a74fcd21ead11c9f5bf5b63454e5e9428a95b
Christina Branson <clbranson@gmail.com>
2016-07-15 22:48:22 +0000

    Added mass assignable properties to models

--- app/Game.php
+++ app/Game.php
@@ -9,5 +9,8 @@ class Game extends Model
     // Set primary key
     protected $primaryKey = 'game_id';
     
+    // Set mass assignable properties
+    protected $fillable = ['player_1_id, player_2_id, winner_id'];
+    
     
 }
--- app/Player.php
+++ app/Player.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Player extends Model
 {
-    //
+    // Set primary key
+    protected $primaryKey = 'player_id';
+    
+    // Set mass assignable properties
+    protected $fillable = ['name, wins, losses'];
 }
