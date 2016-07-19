bd1778c9b8c9916dc44c87ed99b552eb90bb21ad
Christina Branson <clbranson@gmail.com>
2016-07-19 20:40:22 +0000

    Updated routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -25,6 +25,7 @@ Route::post('/game/edit/{game_id}', 'GameController@editGame');
 Route::get('/game/winner/{game_id}', 'GameController@winnerGamePage');
 Route::post('/game/winner/{game_id}', 'GameController@winnerGame');
 Route::get('/game/get/{game_id}', 'GameController@getGame');
+Route::get('/game/list', 'GameController@listGames');
 Route::delete('/game/delete/{game_id}', 'GameController@deleteGame');
 
 // Player management
@@ -33,5 +34,7 @@ Route::post('/player/new', 'PlayerController@newPlayer');
 Route::get('/player/edit/{player_id}', 'PlayerController@editPlayerPage');
 Route::post('/player/edit/{player_id}', 'PlayerController@editPlayer');
 Route::get('/player/get/{player_id}', 'PlayerController@getPlayer');
+Route::get('/player/delete/{player_id}', 'PlayerController@deletePlayerPage');
 Route::delete('/player/delete/{player_id}', 'PlayerController@deletePlayer');
 Route::get('/player/admin', 'PlayerController@admin');
+Route::get('/player/autocomplete/{search}', 'PlayerController@autocomplete');
