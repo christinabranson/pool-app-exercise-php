6b17ca4d5e5bfedca5a776f8438c339292d31bb6
Christina Branson <clbranson@gmail.com>
2016-07-19 20:40:40 +0000

    Added hidden input item to spoof deletion

--- resources/views/players/delete.blade.php
+++ resources/views/players/delete.blade.php
@@ -8,14 +8,14 @@
         @if (count($player) > 0)
         <h1>Delete Player: {{ $player->name }}</h1>
 
-        <form id="deletePlayer" method="DELETE" action="{{ url('player/delete/'.$player->player_id) }}">
+        <form id="deletePlayer" method="POST" action="{{ url('player/delete/'.$player->player_id) }}">
             {{ csrf_field() }}
             
             <p>Are you sure you want to delete <strong>{{ $player->name }}</strong>? This player has been in 
             <strong>{{ sizeof($player->games()) }}</strong> games and all those games will be deleted.</p>
             
             <button type="submit" id="submit" class="alert button">Submit</button>
-            
+            <input type="hidden" name="_method" value="DELETE">
             <p>Player created: {{ $player->created_at }}. Player last modified: {{ $player->updated_at }}.</p>
             
         </form>
