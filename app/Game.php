1b98fc14b6bb6a8aba6f81634cb0a2cd705d2aeb
Christina Branson <clbranson@gmail.com>
2016-07-19 20:40:06 +0000

    Updated models with helper functions

--- app/Game.php
+++ app/Game.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Log;
 use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
@@ -11,6 +12,40 @@ class Game extends Model
     
     // Set mass assignable properties
     protected $fillable = ['player_1_id, player_2_id, winner_id'];
+    
+    // Get player info for both players
+    public function players() {
+        Log::debug("players()");
+        Log::debug("for game id: " .  $this->game_id);
+        $players = Player::findOrFail([$this->player_1_id,$this->player_2_id]);
+        return $players;
+    }
+    
+    // Get player info for player 1
+    public function player1() {
+        Log::debug("player1()");
+        Log::debug("for game id: " .  $this->game_id);
+        $player = Player::findOrFail($this->player_1_id);
+        return $player;
+    }
+    
+    // Get player info for player 2
+    public function player2() {
+        Log::debug("player2()");
+        Log::debug("for game id: " .  $this->game_id);
+        $player = Player::findOrFail($this->player_2_id);
+        return $player;
+    }
+    
+    // Get player info for winner
+    // TODO: Winner might not be set yet so come with a strategy for that
+    public function winner() {
+        Log::debug("winner()");
+        Log::debug("for game id: " .  $this->game_id);
+        $player = Player::find($this->winner_id);
+        return $player;
+    }
+    
     
     
 }
--- app/Player.php
+++ app/Player.php
@@ -2,6 +2,9 @@
 
 namespace App;
 
+use Log;
+use DB;
+use App\Game;
 use Illuminate\Database\Eloquent\Model;
 
 class Player extends Model
@@ -11,4 +14,41 @@ class Player extends Model
     
     // Set mass assignable properties
     protected $fillable = ['name, wins, losses'];
+    
+    // Get player's name
+    // Return with upper character first
+    public function getNameAttribute($value) {
+        return ucfirst($value);
+    }
+    
+    // Get player's games
+    // TODO: Make this work with Eloquent model instead of DB
+    public function games()
+    {
+        Log::debug("games()");
+        Log::debug("for player id: " .  $this->player_id);
+        $games = DB::table('games')
+                    ->where('player_1_id', $this->player_id)
+                    ->orWhere('player_2_id', $this->player_id)
+                    ->get();
+        $games = Game::where('player_1_id', $this->player_id)
+                    ->orWhere('player_2_id', $this->player_id)
+                    ->get();
+        return $games;
+    }
+    
+    // TODO: Function to add to winner's wins
+    public function addWin() {
+        $this->wins = $this->wins + 1;
+        $this->save();
+    }
+    
+    // TODO: Function to add to loser's losse
+    public function addLoss() {
+        $this->losses = $this->losses + 1;
+        $this->save(); 
+    }
+    
+    
+    // TODO: Accessor to modify times to something human readable
 }
