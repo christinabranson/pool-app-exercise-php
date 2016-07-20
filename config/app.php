70ad836378ed095f4380d8b05943879de12810a0
Christina Branson <clbranson@gmail.com>
2016-07-20 13:28:34 +0000

    Update app key

--- config/app.php
+++ config/app.php
@@ -91,7 +91,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', '[base64:GhqwHfqp8H6Hh0J1RHa6vV+MxDppM2/yxdwexMOXfIo=]'),
+    'key' => env('APP_KEY', 'SomeRandomStringSomeRandomString'),
 
     'cipher' => 'AES-256-CBC',
 
